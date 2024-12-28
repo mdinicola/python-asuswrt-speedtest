38b98c5f7cb9e0a00c27b5bc79c542ac2a175001
Mike Di Nicola <mike@mdinicola.com>
2024-12-28 03:36:41 -0500

    first pass at implementing functions

--- run_speedtest.py
+++ run_speedtest.py
@@ -14,31 +14,44 @@ config = configparser.ConfigParser()
 config.read('config/config.ini')
 
 async def asus_get_speedtest_history(client: AsusWrtHttp):
-    pass
+    history = json.loads(await client._AsusWrtHttp__send_req('ookla_speedtest_get_history()'))
+    return history['ookla_speedtest_get_history']
 
 
 def parse_speedtest_history(history: dict, limit: int):
-    pass
+    return history[:limit]
 
 
 async def asus_set_speedtest_start_time(client: AsusWrtHttp, start_time: int):
-    pass
+    data = f'ookla_start_time={start_time}'
+    # data = {"ookla_start_time": start_time}
+    await client._AsusWrtHttp__post(path='set_ookla_speedtest_start_time.cgi', command=data)
 
 
 async def asus_start_speedtest(client: AsusWrtHttp):
-    pass
+    data_type = ""
+    data_id = ""
+    data = f'type={data_type}&id={data_id}'
+    # data = {"type": data_type, "id": data_id}
+    await client._AsusWrtHttp__post(path='ookla_speedtest_exe.cgi', command=data)
 
 
 async def asus_get_speedtest_result(client: AsusWrtHttp):
-    pass
+    result = json.loads(await client._AsusWrtHttp__send_req('ookla_speedtest_get_result()'))
+    return result['ookla_speedtest_get_result']
 
 
-async def wait_and_return_speedtest_result(
-    client: AsusWrtHttp, 
-    timeout: int = config.getint('speedtest', 'default_timeout'), 
-    poll_frequency: int = config.getint('speedtest', 'default_poll_frequency')
-):
-    pass
+async def wait_and_return_speedtest_result(client: AsusWrtHttp, timeout: int, poll_frequency: int):
+    count = 0
+    while(count <= timeout):
+        time.sleep(poll_frequency)
+        count += poll_frequency
+        results = await asus_get_speedtest_result(client)
+        if len(results > 0):
+            latest_result = results[-1]
+            if latest_result['type'] == "result":
+                return latest_result
+    raise Exception(f'Speedtest did not complete within {timeout} seconds')
 
 
 def convert_history_to_payload():
@@ -46,7 +59,20 @@ def convert_history_to_payload():
 
 
 async def run_speedtest():
-    pass
+    client = AsusWrtHttp(
+        config.get('asus_router', 'host'), 
+        config.get('asus_router', 'username'),
+        config.get('asus_router', 'password'), 
+        port = config.getint('asus_router', 'port'),
+        use_https = config.getboolean('asus_router', 'use_https')
+    )
+
+    speedtest_history = parse_speedtest_history(
+        history = await asus_get_speedtest_history(client), 
+        limit = config.getint('speedtest', 'history_limit')
+    )
+
+    await client.async_disconnect()
 
 # asyncio.run(run_speedtest())
 
